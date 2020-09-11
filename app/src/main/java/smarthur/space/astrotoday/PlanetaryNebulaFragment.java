4ad1a6c2d4c722fa79ba2764384e65cb441054d8
arthoviedo <arthoviedo@gmail.com>
2020-09-11 22:54:41 +1000

    Small refactroing, added a new entry for Eskimo Nebula

--- app/src/main/java/smarthur/space/astrotoday/PlanetaryNebulaFragment.java
+++ app/src/main/java/smarthur/space/astrotoday/PlanetaryNebulaFragment.java
@@ -28,6 +28,9 @@ public class PlanetaryNebulaFragment extends Fragment implements UpdatableFragme
 
     private final static String COOKIE_INFO =
             "-33.86785%7C151.20732%7CSydney+%28AU%29%7CAustralia%2FSydney%7C0";
+    private final static String SKY_LIVE_INFO_PREFIX = "https://theskylive.com/sky/deepsky/";
+    private final static String WIKIMEDIA_PREFIX = "https://upload.wikimedia.org/wikipedia/commons";
+
 
     @Override
     public View onCreateView(
@@ -44,71 +47,57 @@ public class PlanetaryNebulaFragment extends Fragment implements UpdatableFragme
     }
 
     public void initPlanetaryNebulae() {
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Saturn Nebula (NGC 7009)",
-                        "https://theskylive.com/sky/deepsky/ngc7009-saturn-nebula-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/c/c9/MUSE_image_of_the_Saturn_Nebula.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Saturn Nebula (NGC 7009)",
+                        SKY_LIVE_INFO_PREFIX + "ngc7009-saturn-nebula-object",
+                        WIKIMEDIA_PREFIX + "/c/c9/MUSE_image_of_the_Saturn_Nebula.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Blinking Planetary (NGC 6826)",
-                        "https://theskylive.com/sky/deepsky/ngc6826-blinking-planetary-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/8/83/NGC_6826HSTFull.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Blinking Planetary (NGC 6826)",
+                        SKY_LIVE_INFO_PREFIX + "ngc6826-blinking-planetary-object",
+                        WIKIMEDIA_PREFIX + "/8/83/NGC_6826HSTFull.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Sprirograph Nebula (IC 418)",
-                        "https://theskylive.com/sky/deepsky/ic418-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/6/6b/Spirograph_Nebula_-_Hubble_1999.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Sprirograph Nebula (IC 418)",
+                        SKY_LIVE_INFO_PREFIX + "ic418-object",
+                        WIKIMEDIA_PREFIX + "/6/6b/Spirograph_Nebula_-_Hubble_1999.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Cat's Eye Nebula (NGC 6543)",
-                        "https://theskylive.com/sky/deepsky/ngc6543-cats-eye-nebula-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/5/5a/NGC6543.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Cat's Eye Nebula (NGC 6543)",
+                        SKY_LIVE_INFO_PREFIX + "ngc6543-cats-eye-nebula-object",
+                        WIKIMEDIA_PREFIX + "/5/5a/NGC6543.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Ghost of Jupiter Nebula (NGC 3242)",
-                        "https://theskylive.com/sky/deepsky/ngc3242-jupiters-ghost-nebula-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/1/14/NGC_3242_%22Ghost_of_Jupiter%22.png"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Ghost of Jupiter Nebula (NGC 3242)",
+                        SKY_LIVE_INFO_PREFIX + "ngc3242-jupiters-ghost-nebula-object",
+                        WIKIMEDIA_PREFIX + "/1/14/NGC_3242_%22Ghost_of_Jupiter%22.png"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Ring Nebula (M 57)",
-                        "https://theskylive.com/sky/deepsky/messier-57-ring-nebula-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/1/13/M57_The_Ring_Nebula.JPG"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Ring Nebula (M 57)",
+                        SKY_LIVE_INFO_PREFIX + "messier-57-ring-nebula-object",
+                        WIKIMEDIA_PREFIX + "/1/13/M57_The_Ring_Nebula.JPG"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Blue Racquetball Nebula (NGC 6572)",
-                        "https://theskylive.com/sky/deepsky/ngc6572-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/c/cd/NGC_6572.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Blue Racquetball Nebula (NGC 6572)",
+                        SKY_LIVE_INFO_PREFIX + "ngc6572-object",
+                        WIKIMEDIA_PREFIX + "/c/cd/NGC_6572.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Blue Flash Nebula (NGC 6905)",
-                        "https://theskylive.com/sky/deepsky/ngc6905-blue-flash-nebula-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/7/70/NGC_6905_-_VLT%28FORS2%29_-_RHaBOIII.png"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Blue Flash Nebula (NGC 6905)",
+                        SKY_LIVE_INFO_PREFIX + "ngc6905-blue-flash-nebula-object",
+                        WIKIMEDIA_PREFIX + "/7/70/NGC_6905_-_VLT%28FORS2%29_-_RHaBOIII.png"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Little Gem Nebula (NGC 6818)",
-                        "https://theskylive.com/sky/deepsky/ngc6818-little-gem-nebula-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/f/f6/Ngc6818.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Little Gem Nebula (NGC 6818)",
+                        SKY_LIVE_INFO_PREFIX + "ngc6818-little-gem-nebula-object",
+                        WIKIMEDIA_PREFIX + "/f/f6/Ngc6818.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Helix Nebula (NGC 7293)",
-                        "https://theskylive.com/sky/deepsky/ngc7293-helix-nebula-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/7/7f/Comets_Kick_up_Dust_in_Helix_Nebula_%28PIA09178%29.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Helix Nebula (NGC 7293)",
+                        SKY_LIVE_INFO_PREFIX + "ngc7293-helix-nebula-object",
+                        WIKIMEDIA_PREFIX + "/7/7f/Comets_Kick_up_Dust_in_Helix_Nebula_%28PIA09178%29.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
-        planetaryNebulaMap.put(
-                new PlanetaryNebula(
-                        "Copeland's Blue Snowball (NGC 7662)",
-                        "https://theskylive.com/sky/deepsky/ngc7662-copelands-blue-snowball-object",
-                        "https://upload.wikimedia.org/wikipedia/commons/7/7e/NGC_7662_%22Blue_Snowball%22.jpg"),
+        planetaryNebulaMap.put(new PlanetaryNebula("Copeland's Blue Snowball (NGC 7662)",
+                        SKY_LIVE_INFO_PREFIX + "ngc7662-copelands-blue-snowball-object",
+                        WIKIMEDIA_PREFIX + "/7/7e/NGC_7662_%22Blue_Snowball%22.jpg"),
+                new PlanetaryNebulaRowView(getContext()));
+        planetaryNebulaMap.put(new PlanetaryNebula("Eskimo Nebula (NGC 2392)",
+                        SKY_LIVE_INFO_PREFIX + "ngc2392-eskimo-nebula-object",
+                        WIKIMEDIA_PREFIX + "/1/16/Ngc2392.jpg"),
+                new PlanetaryNebulaRowView(getContext()));
+        planetaryNebulaMap.put(new PlanetaryNebula("Retina Nebula (IC 4406)",
+                        SKY_LIVE_INFO_PREFIX + "ic4406-object",
+                        WIKIMEDIA_PREFIX + "/1/1a/Retinanebel.jpg"),
                 new PlanetaryNebulaRowView(getContext()));
         int count = 0;
         for (final Map.Entry<PlanetaryNebula, PlanetaryNebulaRowView> entry
@@ -119,12 +108,13 @@ public class PlanetaryNebulaFragment extends Fragment implements UpdatableFragme
             entry.getValue().image.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    FullScreenImageDialogFragment newFragment = FullScreenImageDialogFragment.newInstance(entry.getKey().imageUrl);
+                    FullScreenImageDialogFragment newFragment =
+                            FullScreenImageDialogFragment.newInstance(entry.getKey().imageUrl);
                     FragmentTransaction ft =
                             getParentFragmentManager()
                                     .beginTransaction();
                     ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);;
-                    
+
                     newFragment.show(ft, "dialog");
                 }
             });
